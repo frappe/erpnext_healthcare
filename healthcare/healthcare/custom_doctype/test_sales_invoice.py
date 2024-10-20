ce42338ccae62da5b215d8560c65a4dbb56fd4ac
Anoop Kurungadam <anoop@earthianslive.com>
2024-10-20 21:39:15 +0530

    test: sales invoice override - use globalTestRecords
    (cherry picked from commit ce21bc0432b2ab006d6c5ab85ea2d39d7247d5bb)
    
    # Conflicts:
    #	healthcare/healthcare/custom_doctype/test_sales_invoice.py

--- healthcare/healthcare/custom_doctype/test_sales_invoice.py
+++ healthcare/healthcare/custom_doctype/test_sales_invoice.py
@@ -3,10 +3,16 @@ from frappe.tests.utils import FrappeTestCase
 
 test_records = frappe.get_test_records("Sales Invoice")
 
+EXTRA_TEST_RECORD_DEPENDENCIES = ["Sales Invoice"]
+
 
 class TestSalesInvoice(FrappeTestCase):
 	def test_set_healthcare_services_should_preserve_state(self):
+<<<<<<< HEAD
 		invoice = frappe.copy_doc(test_records[0])
+=======
+		invoice = frappe.copy_doc(self.globalTestRecords["Sales Invoice"][0])
+>>>>>>> ce21bc0 (test: sales invoice override - use globalTestRecords)
 
 		count = len(invoice.items)
 		item = invoice.items[0]
